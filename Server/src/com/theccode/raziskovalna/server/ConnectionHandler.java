0efc34e85c299e6037519c60f7b4a7cd4d539b91
Vid Drobnič <vid.drobnic@icloud.com>
2015-01-24 11:17:56 +0100

    read, write functions implemented

--- Server/src/com/theccode/raziskovalna/server/ConnectionHandler.java
+++ Server/src/com/theccode/raziskovalna/server/ConnectionHandler.java
@@ -1,28 +1,62 @@
 package com.theccode.raziskovalna.server;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ConnectionHandler implements Runnable{
     private Socket socket;
+    private Thread thread;
+    private PrintWriter out;
+    private String id;
+    private String lastMessage = "";
+    private boolean running = true;
 
     public ConnectionHandler(Socket socket) {
+        id = "";
         this.socket = socket;
+        thread = new Thread(this);
+        thread.start();
+
+        try {
+            out = new PrintWriter(socket.getOutputStream(), true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void run() {
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            while (true) {
+            while (running) {
                 String inputLine = in.readLine();
                 if (inputLine == null) break;
+                if (id.equals("")) id = inputLine;
+                else lastMessage = inputLine;
 
                 System.out.println(inputLine);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public synchronized boolean isAlive() {
+        return thread.isAlive();
+    }
+
+    public synchronized void write(String data) {
+        out.println(data);
+    }
+
+    public synchronized String read() {
+        return lastMessage;
+    }
+
+    public synchronized String getId() {
+        return id;
     }
 }
--- Server/src/com/theccode/raziskovalna/server/Server.java
+++ Server/src/com/theccode/raziskovalna/server/Server.java
@@ -1,15 +1,19 @@
 package com.theccode.raziskovalna.server;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Server {
     private int port;
     private boolean run = false;
     private ServerSocket serverSocket;
+    private ArrayList<ConnectionHandler> connectionHandlers;
 
     public Server(int port) {
         this.port = port;
+        connectionHandlers = new ArrayList<ConnectionHandler>();
     }
 
     public Server() {
@@ -24,10 +28,40 @@ public class Server {
             while (run) {
                 Socket socket = serverSocket.accept();
                 ConnectionHandler connectionHandler = new ConnectionHandler(socket);
-                new Thread(connectionHandler).start();
+                connectionHandlers.add(connectionHandler);
+
+                System.out.println(read("mami"));
+                write("mami", "oci");
             }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public String read(String id) {
+        clean();
+        for (int i = 0; i < connectionHandlers.size(); i++) {
+            if (connectionHandlers.get(i).getId().equals(id))
+                return connectionHandlers.get(i).read();
+        }
+
+        return "";
+    }
+
+    public void write(String id, String data) {
+        clean();
+        for (int i = 0; i < connectionHandlers.size(); i++) {
+            if (connectionHandlers.get(i).getId().equals(id))
+                connectionHandlers.get(i).write(data);
+        }
+    }
+
+    private void clean() {
+        for (int i = 0; i < connectionHandlers.size(); i++) {
+            if (!connectionHandlers.get(i).isAlive()) {
+                connectionHandlers.remove(i);
+                i--;
+            }
         }
     }
 }
