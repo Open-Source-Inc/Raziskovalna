b0573a5f3051d5a71b31dea55c063957d5e27c92
Žiga Patačko Koderman <ziga.patacko@gmail.com>
2015-02-08 21:42:38 +0100

    temp

--- Client/CO2.c
+++ Client/CO2.c
@@ -14,24 +14,27 @@ void main(){
 
 	wait();
 
-	i2c_startw_tx();
+
+	I2CRead(0x15, 0x1389, &data1, 2);
+
+	/*i2c_startw_tx();
 	i2c_write_char((char) 0x15);
 	i2c_write_char((char) 0x04);
 	i2c_write_char((char) 0x13);
-	i2c_write_char((char) 0x8A);
+	i2c_write_char((char) 0x89);
 	i2c_write_char((char) 0x00);
 	i2c_write_char((char) 0x01);
-	i2c_start_tx();
-	i2c_write_char((char) 0x16);
-	i2c_write_char((char) 0x04);
 	i2c_read_char(&tempData1);
 	i2c_read_char(&tempData2);
 	i2c_read_char(&data1);
 	i2c_read_char(&data2);
+	i2c_send_nak();
 	i2c_stop_tx(); 
 
 	printf("%d\n", tempData1);
 	printf("%d\n", tempData2);
 	printf("%d\n", data1);
-	printf("%d\n", data2);
+	printf("%d\n", data2);*/
+
+	printf("%d\n", data1);
 }
