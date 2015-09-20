176537e8677cc205c4a10dbec32e53448f54f247
Žiga Patačko Koderman <ziga.patacko@gmail.com>
2015-09-20 15:26:55 +0200

    improved readCo2 method implementation

--- Client/CO2.c
+++ Client/CO2.c
@@ -6,11 +6,10 @@ const char CO2_WR_ADDRESS = 0x2A;
 const char CO2_RD_ADDRESS = 0x2B;
 const char CO2_FUNCTION_CODE = 0x04;
 char co2Data0, co2Data1, co2Data2, co2Data3;
-char co2Status0, co2Status1, co2Status2, co2Status3;
 int waitInt, co2, result;
 
 void wait(){
-	for(waitInt = 0; waitInt < 2000; waitInt++){}
+	for(waitInt = 0; waitInt < 20000; waitInt++){}
 }
 
 //sample method that prints value from co2 sensor
@@ -29,24 +28,24 @@ int readCo2(){
 
 		i2c_write_char(CO2_RD_ADDRESS);
 	
-		i2c_read_char(&co2Status0);
+		i2c_read_char(&co2Data0);
 		i2c_send_ack();
-		i2c_read_char(&co2Status1);
+		i2c_read_char(&co2Data1);
 		i2c_send_ack();
-		i2c_read_char(&co2Status2);
+		i2c_read_char(&co2Data2);
 		i2c_send_ack();
-		i2c_read_char(&co2Status3);
+		i2c_read_char(&co2Data3);
 		i2c_send_nak();
 		i2c_stop_tx();
-		if(co2Status0 != 0){
+		if(co2Data0 != 0){
 			break;
 		}
 	}
-	/*printf("%d, %d, %d, %d: ", co2Status0, co2Status1, co2Status2, co2Status3);
-	result = 256*co2Status2 + co2Status3;
+	/*printf("%d, %d, %d, %d: ", co2Data0, co2Data1, co2Data2, co2Data3);
+	result = 256*co2Data2 + co2Data3;
 	printf("%d\n", result);
 	return result;*/
-	return 256*co2Status2 + co2Status3;
+	return 256*co2Data2 + co2Data3;
 }
 
 void main(){
