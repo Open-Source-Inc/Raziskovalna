bc178beb8cfc5491931762ca028cd380e651f507
Žiga Patačko Koderman <ziga.patacko@gmail.com>
2015-09-20 15:22:03 +0200

    improved readCo2 method implementation

--- Client/CO2.c
+++ Client/CO2.c
@@ -7,39 +7,46 @@ const char CO2_RD_ADDRESS = 0x2B;
 const char CO2_FUNCTION_CODE = 0x04;
 char co2Data0, co2Data1, co2Data2, co2Data3;
 char co2Status0, co2Status1, co2Status2, co2Status3;
-int waitInt, result;
+int waitInt, co2, result;
 
 void wait(){
 	for(waitInt = 0; waitInt < 2000; waitInt++){}
 }
 
+//sample method that prints value from co2 sensor
+int readCo2(){
+	while(1){
+		i2c_start_tx();
+		i2c_write_char(CO2_WR_ADDRESS);
+		i2c_write_char(CO2_FUNCTION_CODE);
+		i2c_write_char(0x13);
+		i2c_write_char(0x8B);
+		i2c_write_char(0);
+		i2c_write_char(1);
+		
+		i2c_stop_tx();
+		i2c_start_tx();
 
-void printCo2SensorStatus(){
-	i2c_start_tx();
-	i2c_write_char(CO2_WR_ADDRESS);
-	i2c_write_char(CO2_FUNCTION_CODE);
-	i2c_write_char(0x13);
-	i2c_write_char(0x8B);
-	i2c_write_char(0);
-	i2c_write_char(1);
+		i2c_write_char(CO2_RD_ADDRESS);
 	
-	i2c_stop_tx();
-	i2c_start_tx();
-
-	i2c_write_char(CO2_RD_ADDRESS);
-	
-	i2c_read_char(&co2Status0);
-	i2c_send_ack();
-	i2c_read_char(&co2Status1);
-	i2c_send_ack();
-	i2c_read_char(&co2Status2);
-	i2c_send_ack();
-	i2c_read_char(&co2Status3);
-	i2c_send_nak();
-	i2c_stop_tx();
-	printf("%d, %d, %d, %d: ", co2Status0, co2Status1, co2Status2, co2Status3);
+		i2c_read_char(&co2Status0);
+		i2c_send_ack();
+		i2c_read_char(&co2Status1);
+		i2c_send_ack();
+		i2c_read_char(&co2Status2);
+		i2c_send_ack();
+		i2c_read_char(&co2Status3);
+		i2c_send_nak();
+		i2c_stop_tx();
+		if(co2Status0 != 0){
+			break;
+		}
+	}
+	/*printf("%d, %d, %d, %d: ", co2Status0, co2Status1, co2Status2, co2Status3);
 	result = 256*co2Status2 + co2Status3;
 	printf("%d\n", result);
+	return result;*/
+	return 256*co2Status2 + co2Status3;
 }
 
 void main(){
@@ -47,7 +54,8 @@ void main(){
 	wait();
 	wait();
 	while(1){
-		printCo2SensorStatus();
+		co2 = readCo2();
+		printf("%d\n", co2);
 		wait();
 		wait();
 		wait();
